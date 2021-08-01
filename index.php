ae594230c687b2a365b825f9f2352fbb282a2d56
vudinhquang <vudinhquangk53@gmail.com>
2021-08-02 01:07:07 +0900

    List

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require_once './libs/Json.php';
+
+$table = './data/group.json';
+$column = ['id', 'name', 'status'];
+$objGroup = new Json($table, $column);
+
+$data = $objGroup->list();
--- libs/Json.php
+++ libs/Json.php
@@ -1,4 +1,15 @@
 <?php
 class Json{
 
+    private $table = '';  
+    private $columns = [];
+
+    public function __construct($table, $columns){
+        $this->table = $table;
+        $this->columns = $columns;
+    }
+
+    public function list(){
+        return json_decode(file_get_contents($this->table), true);
+    }
 }
