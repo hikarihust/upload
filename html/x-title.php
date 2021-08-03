378fa06d75a22a459e789bc5777bcf74e304dc46
vudinhquang <vudinhquangk53@gmail.com>
2021-08-03 23:43:02 +0900

    Change button

--- html/x-title.php
+++ html/x-title.php
@@ -1,5 +1,17 @@
+<?php
+// echo $_SERVER['PHP_SELF'];
+$currentFile = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
+if($currentFile == 'index'){
+    $title = 'Danh sách sản phẩm';
+}elseif($currentFile == 'form'){
+    $title = 'Nhập dữ liệu';
+}elseif($currentFile == 'info'){
+    $title = 'Chi tiết sản phẩm';
+}
+?>
+
 <div class="x_title">
-    <h2>Danh sách</h2>
+    <h2><?php echo $title ?></h2>
     <ul class="nav navbar-right panel_toolbox">
         <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a></li>
         <li><a class="close-link"><i class="fa fa-close"></i></a></li>
