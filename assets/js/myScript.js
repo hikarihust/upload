47bdbd678b7e20e790272f975c6c514674b76ea6
vudinhquang <vudinhquangk53@gmail.com>
2021-08-04 18:29:15 +0900

    Create form 02

--- assets/js/myScript.js
+++ assets/js/myScript.js
@@ -25,16 +25,23 @@ $(".image-upload").change(function () {
     }
 });
 
+var resetLabel = function(){
+    $('.label-image').each(function (i) {
+        $(this).html('Hình ảnh ' + (i+1));
+    });
+}
+
 $('.btn-add-image').click(function (e) { 
     e.preventDefault();
+    var index = $('.label-image').length + 1;
     $element = `
     <div class="item form-group input">
-        <label class="col-form-label col-md-3 col-sm-3 label-align" for="first-name">Hình ảnh phụ 1 
+        <label class="col-form-label col-md-3 col-sm-3 label-align label-image" for="first-name">Hình ảnh `+index+`
         </label>
         <div class="col-md-6 col-sm-6 input-wrapper">
-            <input type="file" id="image_main" name="image_extra[]" class="form-control col-md-6 image-upload">
-            <input type="text" name="alt_extra[]" value="" class="form-control col-md-3 ml-md-0">
-            <input type="number" name="ordering[]" value="" class="form-control col-md-2 ml-md-0">
+            <input type="file" name="images[]" class="form-control col-md-6 image-upload">
+            <input type="text" name="alt[]" placeholder="alt_image" value="" class="form-control col-md-3 ml-md-0">
+            <input type="number" name="ordering[]" placeholder="ordering" value="" class="form-control col-md-2 ml-md-0">
             <button type="button" class="btn btn-danger btn-sm btn-remove-image rounded-circle"><i class="fa fa-times"></i></button>
         </div>
     </div>
@@ -42,8 +49,22 @@ $('.btn-add-image').click(function (e) {
 
     $('.item.form-group.input').last().after($element);
 
+    $(".image-upload").change(function () {
+        var currentImageUpload = $(this);
+        if (this.files && this.files[0]) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                currentImageUpload.parent().find('.preview').remove(); 
+                currentImageUpload.parent().append('<img src="'+e.target.result+'" class="preview"/>');
+                currentImageUpload.parent().find('.btn-remove-image').addClass('switch-btn');
+            };
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+
     $('.btn-remove-image').click(function (e) { 
         e.preventDefault();
-        $(this).closest('.item.form-group.input').remove();
+        var t = $(this).closest('.item.form-group.input');
+        $.when(t.remove()).then(resetLabel());
     });
 });
