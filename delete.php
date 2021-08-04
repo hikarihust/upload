eedc433afc7fe89f096f728765aa83dc410f750e
vudinhquang <vudinhquangk53@gmail.com>
2021-08-04 15:45:04 +0900

    Delete product

--- delete.php
+++ delete.php
@@ -4,10 +4,10 @@ require_once './connect.php';
 
 $result = $obj->delete($_GET['id']);
 
-unlink(PATH_UPLOAD . $result['image_main']);  
+unlink(PATH_UPLOAD . $result['image_main']['image']);  
 
 foreach ($result['image_extra'] as $key => $value) {
-    unlink(PATH_UPLOAD . $value);  
+    unlink(PATH_UPLOAD . $value['image']);  
 }
 
 $_SESSION['message']['success'] = 'Xóa sản phẩm thành công!';
