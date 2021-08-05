486274bf8cc1720c2178f591bedf5859381b119b
vudinhquang <vudinhquangk53@gmail.com>
2021-08-05 09:51:27 +0900

    Delete product

--- delete.php
+++ delete.php
@@ -4,9 +4,7 @@ require_once './connect.php';
 
 $result = $obj->delete($_GET['id']);
 
-unlink(PATH_UPLOAD . $result['image_main']['image']);  
-
-foreach ($result['image_extra'] as $key => $value) {
+foreach ($result['images'] as $key => $value) {
     unlink(PATH_UPLOAD . $value['image']);  
 }
 
